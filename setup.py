4728d38d92a4f0ad67e69710e5b274bfc89512e6
Asheesh Laroia <asheesh@asheesh.org>
2013-09-24 00:33:46 -0500

    Adding setup.py and trivial __init__

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='BuildEverything',
+    version='0.1dev',
+    packages=['build_everything'],
+    license='CC zero',
+    long_description=open('README').read(),
+)
